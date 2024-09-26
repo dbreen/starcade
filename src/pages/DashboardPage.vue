b73ed5a2cdd8a2ac949a8da5994d426b90d3709d
Dan Breen <dan@ostinc.net>
2024-09-26 18:10:23 -0400

    Initial commit

---
+++ src/pages/DashboardPage.vue
@@ -0,0 +1,23 @@
+<template>
+  <q-page class="flex flex-center dashboard-container">
+    <img
+      alt="Starcade logo"
+      src="~assets/starcade-nobg.png"
+      style="width: 200px; height: 200px"
+    />
+    <q-btn class="glossy" rounded color="deep-orange" label="Play" />
+  </q-page>
+</template>
+
+<script setup>
+defineOptions({
+  name: "DashboardPage",
+});
+</script>
+
+<style lang="scss" scoped>
+.dashboard-container {
+  min-height: 100vh; /* Ensure it covers the full viewport height */
+  background: radial-gradient(circle, $secondary, $primary);
+}
+</style>
